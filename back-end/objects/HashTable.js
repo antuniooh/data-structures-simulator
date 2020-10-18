74af01f2c64ecbf084a2d96bdd545496d29f0eb0
henriquevital00 <henriquevital1000@hotmail.com>
2020-10-18 19:58:09 -0300

    Changed hash table to class

--- back-end/objects/HashTable.js
+++ back-end/objects/HashTable.js
@@ -5,66 +5,59 @@
  * @description : hash
  */
 
-var HashTable = function() {
-  this._storage = [];
-  this._count = 0;
-  this._limit = 8;
+class HashTable{
+  constructor(){
+    this._storage = [];
+    this._count = 0;
+    this._limit = 8;
+  }
+
+  insert(key, value) {
+    let index = this.hash(key, this._limit);
+    if (! this._storage[index]){
+      this._count++;
+    }
+    this._storage[index] = value;
+    if (this._count > this._storage){
+      this.resize(this._limit * 2)
+    }
+  }
+
+  clear() {
+    this._storage = [];
+  }
+
+  remove(key) {
+    let index = this.hash(key, this._limit);
+
+    if (this._storage[index]){
+      this._storage[index] = [];
+    }
+  }
+
+  search(key) {
+    let index = this.hash(key, this._limit);
+    if (this._storage[index]){
+      console.log(this._storage[index]);
+    }
+  }
+
+
+  hash(key, max) {
+    return key % max;
+  }
+
+
+  resize(newLimit) {
+    let oldArray = this._storage;
+
+    this._limit = newLimit;
+    this._count = 0;
+    this._storage = [];
+
+    for (var i = 0; i < oldArray.length; i++) {
+      var tuple = oldArray[i];
+      this.insert(tuple[0], tuple[1]);
+     }
+  };
 }
-
-
-HashTable.prototype.insert = function(key, value) {
-  let index = this.hash(key, this._limit);
-  if (! this._storage[index]){
-    this._count++;
-  }
-  this._storage[index] = value;
-  if (this._count > this._storage){
-    this.resize(this._limit * 2)
-  }
-};
-
-HashTable.prototype.clear = function() {
-  this._storage = [];
-}
-
-HashTable.prototype.remove = function(key) {
-  let index = this.hash(key, this._limit);
-
-  if (this._storage[index]){
-    this._storage[index] = [];
-  }
-};
-
-HashTable.prototype.search = function(key) {
-  let index = this.hash(key, this._limit);
-  if (this._storage[index]){
-    console.log(this._storage[index]);
-  }
-};
-
-
-HashTable.prototype.hash = function(key, max) {
-  return key % max;
-};
-
-
-HashTable.prototype.resize = function(newLimit) {
-  let oldArray = this._storage;
-
-  this._limit = newLimit;
-  this._count = 0;
-  this._storage = [];
-
-  for (var i = 0; i < oldArray.length; i++) {
-    var tuple = oldArray[i];
-    this.insert(tuple[0], tuple[1]);
-   }
-};
-
-
-/******************************TESTS*******************************/
-
-var hashT = new HashTable();
-
-hashT.insert(10, '510-599-1930');
-
