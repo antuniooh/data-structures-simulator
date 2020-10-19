d1377920072064674539fe7da37521527d027bfb
henriquevital00 <henriquevital1000@hotmail.com>
2020-10-19 19:42:17 -0300

    Fixed insert method

--- back-end/objects/Ldde.js
+++ back-end/objects/Ldde.js
@@ -18,72 +18,73 @@ class Ldde {
       this.finded_value= null;
       this.size = 0;
    }
+
    insert(value){
-      let newNode = new Node();
-      newNode.value = value;
-      if (this.size == 0){
-         this.first = newNode;
-      }
-      else{
-         let ptrPrev = null;
-         let ptrAtual = this.first;
-         while(ptrAtual != null && ptrAtual.value < value){
-            ptrPrev = ptrAtual;
-            ptrAtual = ptrAtual.next;
-         }
+    let novo = new Node();
+    novo.value = value;
+    let ptrAnt = null;
+    let ptrAtual = this.first;
 
-         if (ptrPrev) {
-            ptrPrev.next = newNode;
-         }
-         else{
-            this.first = newNode;
-         }
-         newNode.next = ptrAtual;
+    while (ptrAtual != null && ptrAtual.value < value) {
+      ptrAnt = ptrAtual;
+      ptrAtual = ptrAtual.next;
+    }
 
-         if (ptrAtual){
-            ptrAtual.previous = newNode;
-         }
-         else{
-            this.last = newNode;
-         }
-         newNode.previous = ptrPrev;
+    if (ptrAnt) {
+      ptrAnt.next = novo;
+    } else {
+      this.first = novo;
+    }
+    novo.next = ptrAtual;
 
-         this.size++;
+    if (ptrAtual) {
+      ptrAtual.previous = novo;
+    } else {
+      this.last = novo;
+    }
+    novo.previous = ptrAnt;
 
-         return true;
-
-      }
+    this.size++;
+    return true;
    }
-   remove(value){
-      if (this.search(value)){
-         if (this.finded_value.previous){
-            this.finded_value.previous.next = this.finded_value.next;
-            if (this.finded_value.next){
-               this.finded_value.next.previous = this.finded_value.previous;
-            }
-            this.size--;
-         }
-         else{
-            if (this.finded_value.next){
-               this.finded_value.next.previous = null;
-               this.size--;
-            }
-            else{
-               this.first = null;
-               this.last = null;
-               this.size=0;
-            }
-         }
-         return true;
-      }
 
+   remove(value){
+     if(!this.search(value))
       return false;
+
+      let it = this.first;
+
+      while(it != null){
+      if(it.value == value){
+        if(it.previous == null && it.next == null){
+            this.first = nullptr;
+            this.last = nullptr;
+        }
+        else if(it.previous == null){
+            it.next.previous = null;
+            this.first = it.next;
+        }
+        else if (it.next == null){
+          it.previous.next = null;
+          this.last = it.previous;
+        }
+        else{
+          it.previous.next = it.next;
+          it.next.previous = it.previous;
+        }
+        this.size--;
+        return true;
+      }
+      it = it.next;
+    }
+    return false;
    }
 
    clear(){
       this.first = null;
       this.last = null;
    }
+
    search(value){
       let ptrPrev = null;
       let ptrAtual = this.first;
@@ -98,5 +99,4 @@ class Ldde {
       return false;
    }
 }
-
 
